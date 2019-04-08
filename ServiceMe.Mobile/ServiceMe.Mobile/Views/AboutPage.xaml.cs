b7e6b1b92ccf406b906aae6f17690526681ec715
prageethmadhu <prageeth.madhu@gmail.com>
2019-04-08 19:30:16 +0530

    Add project files.

---
+++ ServiceMe.Mobile/ServiceMe.Mobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ServiceMe.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
